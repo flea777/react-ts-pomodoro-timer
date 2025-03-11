b5193ff0e981b63c2b3bd3f24990b935d4473491
Pedro <pedrodinizz2@outlook.com>
2025-03-11 17:06:32 -0300

    routes configurated

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { Button } from './components/Button'
 import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from './styles/themes/default';
-import { GlobalStyle } from './styles/global';
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 export const App = () => {
-  return ( 
+  return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary"/>
-      <Button variant="secondary"/>
-      <Button variant="success"/>
-      <Button variant="danger"/>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
-   );
+  );
 }
