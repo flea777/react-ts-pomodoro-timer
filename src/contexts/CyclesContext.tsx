cb1287eb1ba512a4dca58740b7e3a5f3ad1b963b
Pedro <pedrodinizz2@outlook.com>
2025-03-28 18:08:51 -0300

    immer

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, ReactNode, useReducer, useState } from "react";
 import { Cycle, cyclesReducer } from '../reducers/cycles/reducer'
-import { ActionTypes, addNewCycleAction, interruptCurrentCycleAction, markCurrentCycleAsFinishedAction } from "../reducers/cycles/actions";
+import { addNewCycleAction, interruptCurrentCycleAction, markCurrentCycleAsFinishedAction } from "../reducers/cycles/actions";
 
 interface CreateCycleData {
   task: string
